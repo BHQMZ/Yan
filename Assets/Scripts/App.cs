fe6ac4eb529d27f1848dbefec2df9939aece4d1f
BHQMZ <791019127@qq.com>
2023-11-27 01:06:07 +0800

    <feature> Nakama提交

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,22 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nakama;
 using UnityEngine;
 
 public class App : MonoBehaviour
 {
-    private GameApp _gameApp;
+    public NakamaConnection NakamaConnection;
     
+    private readonly Queue<Action> _executionQueue = new Queue<Action>();
+    
+    private GameApp _gameApp;
+
     private void Awake()
     {
         DontDestroyOnLoad(this);
-        
-        _gameApp = new GameApp();
-        _gameApp.Open();
+ 
+        // _gameApp = new GameApp();
+        // _gameApp.Open();
     }
 
     private void Update()
     {
+        UpdateExecution();
         _gameApp?.Update(Time.deltaTime, Time.frameCount);
     }
 
@@ -28,5 +35,62 @@ public class App : MonoBehaviour
     private void OnDestroy()
     {
         _gameApp?.Destroy();
+    }
+
+    private void UpdateExecution()
+    {
+        lock(_executionQueue) {
+            while (_executionQueue.Count > 0) {
+                _executionQueue.Dequeue().Invoke();
+            }
+        }
+    }
+    
+    private void Enqueue(IEnumerator action) {
+        lock (_executionQueue) {
+            _executionQueue.Enqueue(() => {
+                StartCoroutine(action);
+            });
+        }
+    }
+    
+    private void Enqueue(Action action)
+    {
+        Enqueue(ActionWrapper(action));
+    }
+    
+    IEnumerator ActionWrapper(Action a)
+    {
+        a();
+        yield return null;
+    }
+
+
+    private async void Start()
+    {
+        await NakamaConnection.Connect();
+        
+        NakamaConnection.Socket.ReceivedMatchmakerMatched += m => Enqueue(() => OnReceivedMatchmakerMatched(m));
+        NakamaConnection.Socket.ReceivedMatchPresence += m => Enqueue(() => OnReceivedMatchPresence(m));
+        NakamaConnection.Socket.ReceivedMatchState += m => Enqueue(async () => await OnReceivedMatchState(m));
+        
+        await NakamaConnection.FindMatch();
+    }
+    
+    private async void OnReceivedMatchmakerMatched(IMatchmakerMatched matched)
+    {
+    }
+    
+    private void OnReceivedMatchPresence(IMatchPresenceEvent matchPresenceEvent)
+    {
+    }
+
+    private async Task OnReceivedMatchState(IMatchState matchState)
+    {
+        var userSessionId = matchState.UserPresence.SessionId;
+        switch (matchState.OpCode)
+        {
+            
+        }
     }
 }
